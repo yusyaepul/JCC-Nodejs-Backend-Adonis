7754520d2e162d44b09e00f8a1e6da61dd0b5e5c
yusfreak <yusyaepul@gmail.com>
2021-08-31 18:07:36 +0700

    Revisi switch case assign

--- Tugas 2 - String Conditional/Conditional/tugas_switch_case.js
+++ Tugas 2 - String Conditional/Conditional/tugas_switch_case.js
@@ -1,53 +1,64 @@
-var hari = 29;
-var bulan = 11;
-var tahun = 1992;
+var hari = 1;
+var bulan = 12;
+var tahun = 2000;
 
-if(hari > 31 || hari < 1){
-    console.log('masukkan tanggal antara 1 - 31');
-} else if(bulan > 12 || bulan < 1){
-    console.log('masukkan bulan antara 1 - 12');
-} else if(tahun > 2200 || tahun < 1900){
-    console.log('masukkan tahun antara 1900 - 2200');
-} else {
-    switch(bulan){
-        case 1:
-            console.log(hari+' Januari '+tahun);
-            break;
-        case 2:
-            console.log(hari+' Februari '+tahun);
-            break;
-        case 3:
-            console.log(hari+' Maret '+tahun);
-            break;
-        case 4:
-            console.log(hari+' April '+tahun);
-            break;
-        case 5:
-            console.log(hari+' Mei '+tahun);
-            break;
-        case 6:
-            console.log(hari+' Juni '+tahun);
-            break;
-        case 7:
-            console.log(hari+' Juli '+tahun);
-            break;
-        case 8:
-            console.log(hari+' Agustus '+tahun);
-            break;
-        case 9:
-            console.log(hari+' September '+tahun);
-            break;
-        case 10:
-            console.log(hari+' Oktober '+tahun);
-            break;
-        case 11:
-            console.log(hari+' November '+tahun);
-            break;
-        case 12:
-            console.log(hari+' Desember '+tahun);
-            break;
-        default:
-            console.log('Tidak terjadi apa-apa!');
-            break;
-    }
+switch(true){
+    case hari > 31 || hari < 1:
+        hari = 'Masukkan tanggal antara 1 - 31';
+        break;
 }
+
+switch(true){
+    case tahun > 2200 || tahun < 1900:
+        tahun = 'Masukkan tahun antara 1900 - 2200';
+        break;
+}
+
+switch(bulan){
+    case 1:
+        bulan = 'Januari';
+        break;
+    case 2:
+        bulan = 'Februari';
+        break;
+    case 3:
+        bulan = 'Maret';
+        break;
+    case 4:
+        bulan = 'April';
+        break;
+    case 5:
+        bulan = 'Mei';
+        break;
+    case 6:
+        bulan = 'Juni';
+        break;
+    case 7:
+        bulan = 'Juli';
+        break;
+    case 8:
+        bulan = 'Agustus';
+        break;
+    case 9:
+        bulan = 'September';
+        break;
+    case 10:
+        bulan = 'Oktober';
+        break;
+    case 11:
+        bulan = 'November';
+        break;
+    case 12:
+        bulan = 'Desember';
+        break;
+    default:
+        bulan = 'Masukkan bulan anatara 1 - 12';
+}
+
+return console.log(hari+' '+bulan+' '+tahun);
+
+/* 
+Output : 1 Desember 2000
+Jika bulan 0 atau 13, Output bulan jadi : Masukkan bulan anatara 1 - 12
+Jika tahun lebih besar dari 2200 atau kurang dari 1900 Output tahun jadi : Masukkan tahun antara 1900 - 2200
+*/
